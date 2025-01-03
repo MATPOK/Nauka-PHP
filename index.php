9a8ad86b3ad94f94358ae9d1d7a8607c19af2a55
Mateusz Pokrywka <177143@o365.prz.edu.pl>
2025-01-04 00:36:40 +0100

    Lekcja 22 skończona bazy danych aktualizacja dodawanie usuwanie.

--- index.php
+++ index.php
@@ -11,59 +11,19 @@
 </head>
 
 <body>
-    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-        <input type="number" name="num01" placeholder="Number one" required />
-        <select name="operator">
-            <option value="add">+</option>
-            <option value="subtract">-</option>
-            <option value="multiply">*</option>
-            <option value="divide">/</option>
-        </select>
-        <input type="number" name="num02" placeholder="Number two" required />
-        <button>Calculate</button>
+   <h3>Update</h3>
+    <form action="includes/userupdate.inc.php" method="post">
+        <input type="text" name="username" placeholder="Username" />
+        <input type="password" name="pwd" placeholder="Password" />
+        <input type="text" name="email" placeholder="E-Mail" />
+        <button>Update</button>
     </form>
 
-    <?php 
-    if($_SERVER["REQUEST_METHOD"] == "POST") {
-        $num01 = filter_input(INPUT_POST, "num01", FILTER_SANITIZE_NUMBER_FLOAT);
-        $num02 = filter_input(INPUT_POST, "num02", FILTER_SANITIZE_NUMBER_FLOAT);
-        $operator = htmlspecialchars($_POST["operator"]);
-
-        $errors = false;
-        if (empty($num01) || empty($num02) || empty($operator)) {
-            echo "<p class='calc-error'>Fill in all fields!</p>";
-            $errors = true;
-        }
-
-        if (!is_numeric($num01) || !is_numeric($num01)){
-            echo "<p class='calc-error'>Only write numbers!</p>";
-            $errors = true;
-        }
-
-        if (!$errors) {
-            $value = 0.0;
-            switch ($operator) {
-                case "add":
-                    $value = $num01 + $num02;
-                    break;
-
-                case "subtract":
-                    $value = $num01 - $num02;
-                    break;
-
-                case "multiply":
-                    $value = $num01 * $num02;
-                    break;
-
-                case "divide":
-                    $value = $num01 / $num02;
-                    break;
-                default:
-                    echo "<p class='calc-error'>Something went wrong!</p>";
-            }
-            echo "<p class='calc-resoult'>Resoult = " . $value . "</p>";
-        }
-    }
-    ?>
+    <h3>Delete</h3>
+    <form action="includes/userdelete.inc.php" method="post">
+        <input type="text" name="username" placeholder="Username" />
+        <input type="password" name="pwd" placeholder="Password" />
+        <button>Delete</button>
+    </form>
 </body>
 </html>
